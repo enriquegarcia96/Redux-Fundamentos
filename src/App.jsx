2af71c82210d991e41680baecd158b551802f160
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-03-29 14:14:49 -0600

    Creacion de un nuevo metodo para Proteger la Ruta

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,67 @@ import React from 'react';
 import Pokemones from './components/Pokemones';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import { auth } from './firebase';
 
-import { BrowserRouter as Router, Switch, Route }  from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect }  from 'react-router-dom';
 
 function App() {
 
-  return (  
+  const [firebaseUser, setFirebaseUser ] = React.useState(false);
+
+  React.useEffect( () =>{
+
+    const fetchUser = () => {
+
+        auth.onAuthStateChanged( user => {
+          console.log(user)
+    
+          if (user) {
+            setFirebaseUser(user);
+          }else{
+            setFirebaseUser(null);
+          }
+        })
+    }
+
+    fetchUser()
+
+  }, [] )
+
+
+  //*--- para poteger la ruta NUEVO METODO ---*//
+  const RutaPrivada = ({component, path, ...rest}) =>{
+
+    if (localStorage.getItem('usuario')) {
+      const usuarioStorage = JSON.parse(localStorage.getItem('usuario'))
+
+      if (usuarioStorage.uid === firebaseUser.uid ) {
+        return <Route component={component} path={path} {...rest} />
+      }else{
+        return <Redirect to="/login" {...rest}  />
+      }
+
+    }else{
+      return <Redirect to="/login" {...rest}  />
+    }
+  }
+
+  
+
+  return firebaseUser !== false ? (  
     <Router>
         <div className="container mt-3">
 
             <Navbar />
 
             <Switch>
-              <Route component={Pokemones} path="/" exact/>
+              <RutaPrivada component={Pokemones} path="/" exact/>
               <Route component={Login} path="/login" />    
             </Switch>
 
         </div>
     </Router>
-  );
+  ) : (<div><h3>Cargando...</h3></div>)
 }
 
 export default App;
