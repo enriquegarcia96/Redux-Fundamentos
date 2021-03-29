57750acbf4f607b805c32b8007426dff7e69763a
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-03-28 21:54:05 -0600

    Configuracion de firebase y CDN

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const  firebaseConfig = {
+    apiKey: "AIzaSyAEP1-yuu0ncAbEtRH_zVreU505K0u9rwQ",
+    authDomain: "pokemon-redux-auth-storage.firebaseapp.com",
+    projectId: "pokemon-redux-auth-storage",
+    storageBucket: "pokemon-redux-auth-storage.appspot.com",
+    messagingSenderId: "454571014780",
+    appId: "1:454571014780:web:fcf8da8b4253d4458967f9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+
+export {auth, firebase}
