c4c996a8812db3c16bf2e1e3b1b236a2c2ae6972
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-03-29 13:09:47 -0600

    se agrego el dispatch de ingreso usuario accion

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { ingresoUsuarioAcccion } from '../redux/usuarioDuck';//la accion
 
 const Login = () => {
+
+    const dispatch = useDispatch();
+
+
     return (
         <div className="mt-5 text-center">
             <h3 className="text-uppercase font-italic">Ingreso con Google</h3>
             <hr/>
-            <button className="btn btn-primary text-uppercase fab fa-google"> Acceder</button>
+            <button 
+                className="btn btn-primary text-uppercase fab fa-google "
+                onClick={ () => dispatch(ingresoUsuarioAcccion())}
+            > Acceder
+            </button>
             
         </div>
     )
