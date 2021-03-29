d48c5bc5be1f1ec01c2577c065c05b75f0d506cf
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-03-29 14:16:19 -0600

    ocultar los botones y configuraciones finales

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { useDispatch, useSelector }  from 'react-redux';
 
-const Navbar = () => {
+import {cerrarSesionAccion}  from '../redux/usuarioDuck.js';
+import { withRouter } from 'react-router-dom';
+
+
+const Navbar = ( props ) => {
+
+    const dispatch = useDispatch();
+    const activo = useSelector( store => store.usuario.activo )
+
+    //para empujar a la pagina de login
+    const cerrarSesion = () => {
+
+        dispatch(cerrarSesionAccion())
+        props.history.push('/login')
+
+    }
+
+
+
     return (
         <div className="navbar navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">APP POKEMON</Link>
             <div className="d-flex">
-                <NavLink className="btn btn-outline-info mr-2" to="/" exact>Inicio</NavLink>
-                <NavLink className="btn btn-outline-info mr-2" to="/login" exact>Login</NavLink>
-                <button className="btn btn-outline-danger mr-2">Cerrar Sesión</button>
+                {
+                    activo ? (
+                        <>
+                            <NavLink className="btn btn-outline-info mr-2" to="/" exact>Inicio</NavLink>
+                            <button 
+                                className="btn btn-outline-danger mr-2"
+                                onClick={ () =>  cerrarSesion() }
+                            >
+                            Cerrar Sesión
+                            </button>
+                        </>
+                    ): (
+                        <NavLink className="btn btn-outline-info mr-2" to="/login" exact>Login</NavLink>
+                    )
+                }
             </div>
         </div>
     )
 }
 
-export default Navbar
+export default withRouter (Navbar)
